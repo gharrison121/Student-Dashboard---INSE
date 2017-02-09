596fec557a1b20451baa37ff7afd54fd70af335a
gharrison121 <gharrison121@gmail.com>
2017-02-09 19:30:27 +0000

    Added Comments

--- INSE DB.sql
+++ INSE DB.sql
@@ -1,18 +1,18 @@
--- Potential tables
-
-
 CREATE TABLE IF NOT EXISTS STUDENT (
 stu_ID INT PRIMARY KEY AUTO_INCREMENT,
 stu_username VARCHAR(30) NOT NULL UNIQUE,
 stu_password VARCHAR(32) NOT NULL,
 stu_upemail VARCHAR(45) NOT NULL UNIQUE
 );
+
+--Dummy record for database
 INSERT INTO STUDENT (stu_username, stu_password, stu_upemail) VALUES 
 ('student1', MD5('secretpassword'),'up771451@myport.ac.uk');
 
+-- Test query
 SELECT * FROM STUDENT WHERE stu_username='student1' AND stu_password=MD5('secretpassword');
 
-
+-- Unsure if unit table required, many to many splitter needed between student and unit, would require full *dummy* db containing unit names
 CREATE TABLE IF NOT EXISTS UNIT (
 unit_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 unit_name VARCHAR(55) NOT NULL
