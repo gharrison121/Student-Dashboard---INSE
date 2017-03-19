2b24d323906d1010ff59640cf5e724fd27249fd1
gharrison121 <gharrison121@gmail.com>
2017-03-19 15:04:56 +0000

    DB SQL Changes
    Table changes, more consistent with design

--- INSE DB.sql
+++ INSE DB.sql
@@ -8,25 +8,21 @@ stu_password VARCHAR(32) NOT NULL,
 stu_upemail VARCHAR(45) NOT NULL UNIQUE
 );
 
--- Unsure if unit table required, many to many splitter needed between student and unit, would require full *dummy* db containing unit names
-CREATE TABLE IF NOT EXISTS UNIT (
-unit_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-unit_name VARCHAR(55) NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS SCHEDULEWEEK (
--- This is for scheduled tasks that are repeated week upon week.
-sw_ID INT PRIMARY KEY AUTO_INCREMENT
-sw_day -- schedule day
-sw_startTime TIME -- schedule task start time
-sw_endTime TIME -- schedule task end time
-sw_name VARCHAR(150)
-sw_room VARCHAR(10)
-sw_startDate DATE -- the first day the task occurs
-sw_endDate DATE -- the last day the task occurs
+CREATE TABLE IF NOT EXISTS TEACHINGBLOCK (
+tb_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+tb_name VARCHAR(55) NOT NULL,
+tb_startTime DATETIME NOT NULL, 
+tb_endTime DATETIME NOT NULL,
+tb_room VARCHAR(10)
 );
 
 
+CREATE TABLE IF NOT EXISTS REMINDER (
+rem_ID INT PRIMARY KEY AUTO_INCREMENT,
+rem_time DATETIME NOT NULL,
+rem_description VARCHAR(200) NOT NULL
+);
 --Dummy record for database
 INSERT INTO STUDENT (stu_username, stu_password, stu_upemail) VALUES 
 ('student1', MD5('secretpassword'),'up771451@myport.ac.uk');
