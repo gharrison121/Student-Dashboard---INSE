3ade50b29439cb60e7c1846033a9e4a49ea35381
gharrison121 <gharrison121@gmail.com>
2017-04-12 12:59:38 +0100

    Name Fixes

---
+++ Dashboard/Dashboard/LogIn.vb
@@ -0,0 +1,63 @@
+﻿Public Class LogIn
+
+    Private Sub LogIn_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.DarkViolet
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles UserNameInput.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub LogInButton_Click(sender As Object, e As EventArgs) Handles LogInButton.Click
+        LogIn(UserNameInput, PasswordInput)
+    End Sub
+
+    Public Sub LogIn(usernameInp As TextBox, passwordInp As TextBox)
+        ' lets grab our user's credentials
+        Dim username As String = usernameInp.Text
+        Dim password As String = passwordInp.Text
+        ' first we'll make sure we have a valid input, and if not ask 
+        ' the user to correct it
+        If passwordInp.TextLength <= 0 Then
+            MessageBox.Show("Please enter your Student Id to login", "Portsmouth University", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            UserNameInput.Focus()
+            Return
+        ElseIf passwordInp.TextLength <= 0 Then
+            MessageBox.Show("Please enter your Password to login", "Portsmouth University", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            PasswordInput.Focus()
+            Return
+        End If
+        ' now that we know there's appropriate data, lets verify it
+        If VerifyCredentials(username, password) Then
+            Me.Hide()
+            Main.Show()
+            Main.Username.Text = username
+        Else
+            LogInError.Visible = True
+            usernameInp.Text = ""
+            passwordInp.Text = ""
+            usernameInp.Focus()
+        End If
+    End Sub
+
+    Public Function VerifyCredentials(username As String, password As String) As Boolean
+        Return username.Equals("up787321") And password.Equals("hey")
+    End Function
+
+    Private Sub RegisterButton_Click(sender As Object, e As EventArgs) Handles RegisterButton.Click
+        Me.Hide()
+        Register.Show()
+    End Sub
+
+    Private Sub PasswordInput_TextChanged(sender As Object, e As EventArgs) Handles PasswordInput.TextChanged
+
+    End Sub
+End Class
---
+++ Dashboard/DashboardTester/DashboardTest.vb
@@ -0,0 +1,74 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports System.Windows
+Imports StudentDashboard
+
+<TestClass()> Public Class DashboardTest
+
+    ' Represents a dummy and theoretical environment of our application and its forms, 
+    ' providing a class instance of data tracked by the title o the form - may access 
+    ' data using the below GetForm(title) function
+    Dim TestForms(,) As Object = New Object(,) {
+        {"Bus", New Bus},
+        {"Log In", New LogIn},
+        {"Main", New Main},
+        {"NewClass", New NewClass},
+        {"Register", New Register},
+        {"Reminder", New Reminder},
+        {"Timetable", New Timetable}
+    }
+
+    'Private Sub InitForms()
+    '    Dim index As Integer = 0
+    '    TestForms(index, index) = {"Bus", New Bus}
+    '    index = index + 1
+    '    TestForms(index, index) = {"Log In", New LogIn}
+    '    index = index + 1
+    '    TestForms(index, index) = {"Main", New Main}
+    '    index = index + 1
+    '    TestForms(index, index) =
+    '    index = index + 1
+    '    TestForms(index, index) = {"Register", New Register}
+    '    index = index + 1
+    '    TestForms(index, index) = {"Reminder", New Reminder}
+    '    index = index + 1
+    '    TestForms(index, index) = {"Timetable", New Timetable}
+    'End Sub
+
+    ' Allows us to grab a form of choice from our dummy environment, using the 
+    ' title of the form desired
+    Private Function GetForm(title As String)
+        For Each element As Object In TestForms
+            Dim name As String = element(0)
+            If name.Equals(title) Then
+                Dim src = element(1)
+                Return src
+            End If
+        Next
+        Return Nothing
+    End Function
+
+    <TestMethod()> Public Sub TestLogIn()
+        Dim logIn As LogIn = GetForm("LogIn") ' get the log in form
+        ' generate some test data
+        Dim testDataName As String = "up787321"
+        Dim testDataPass As String = "hey"
+        ' simulate a user entering their details into the form
+        logIn.UserNameInput.Text = testDataName
+        logIn.PasswordInput.Text = testDataPass
+        Assert.AreEqual(testDataName, logIn.UserNameInput, True)
+        Assert.AreEqual(testDataPass, logIn.PasswordInput, False)
+        ' lets find out if the program likes out information..
+        Assert.IsTrue(logIn.VerifyCredentials(testDataName, testDataPass))
+        ' .. and lets simulate the user submitting their details
+        logIn.LogIn(logIn.UserNameInput, logIn.PasswordInput)
+        Assert.AreEqual(testDataName, GetForm("Main").UserName.Text())
+    End Sub
+
+    ' can't be tested until we have database or something similar
+    <TestMethod()> Public Sub TestRegistration()
+        Dim register As Register = GetForm("Register")
+
+    End Sub
+
+End Class
